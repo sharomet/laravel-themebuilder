0b104ce0c0ad3236a99751e42dc151884794daa6
sharomet <sharometal@gmail.com>
2019-02-13 16:56:11 +0200

    Added color picker

--- resources/js/components/edit/forms/ThemeColors.vue
+++ resources/js/components/edit/forms/ThemeColors.vue
@@ -2,15 +2,18 @@
     <FormContainerTemplate :title="'Theme Colors'" :id="'themeColors'" :isShow="true">
 
         <div class="col-12" v-for="(variable, index) of variables" :key="index">
+            <chrome-picker v-if="variable.color" v-model="variable.color"></chrome-picker>
             <div class="form-group">
                 <label :for="variable.name">{{ variable.label }}</label>
+                {{ variable.color.hex }}
                 <input type="text"
                        :id="variable.name"
                        class="form-control"
                        :name="variable.name"
-                       :value="GET_VARIABLES[variable.name]"
+                       :value="variable.color.hex ? variable.color.hex : variable.color.rgba"
                        @input="setVar">
             </div>
+            {{ GET_VARIABLES[variable.name] }}
         </div>
 
     </FormContainerTemplate>
@@ -18,41 +21,54 @@
 </template>
 
 <script>
+    import { Chrome } from 'vue-color';
     import { mapGetters, mapActions } from 'vuex';
     import FormContainerTemplate from './FormContainerTemplate'
     export default {
         components: {
-            FormContainerTemplate
+            FormContainerTemplate,
+            'chrome-picker': Chrome
         },
         data: () => {
             return {
                 variables: [
                     {
                         'label': 'Primary Color',
-                        'name': 'base-color-primary'
+                        'name': 'base-color-primary',
+                        'color': '#000'
                     },
                     {
                         'label': 'Secondary Color',
-                        'name': 'base-color-second'
+                        'name': 'base-color-second',
+                        'color': '#000'
                     }
                 ]
             }
         },
         computed: {
             ...mapGetters(['GET_VARIABLES']),
-            /*baseColorPrimary: {
-                get() {
-                    return this.GET_VARIABLES['base-color-primary'];
-                },
-                set(value) {
-                    this.SET_VAR(['base-color-primary', value]);
-                }
-            },*/
+        },
+        mounted () {
+
         },
         methods: {
             ...mapActions(['SET_VAR']),
             setVar(e) {
                 this.SET_VAR([e.target.name, e.target.value]);
+            },
+            addItems(){
+                return [
+                    {
+                        'label': 'Primary Color',
+                        'name': 'base-color-primary',
+                        'color': this.GET_VARIABLES['base-color-primary']
+                    },
+                    {
+                        'label': 'Secondary Color',
+                        'name': 'base-color-second',
+                        'color': this.GET_VARIABLES['base-color-second']
+                    }
+                ]
             }
         }
     }
